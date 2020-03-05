51b46ad3d70b64e1380e1568b8e394658ea72543
Kame <ev60537@meiji.ac.jp>
2020-03-05 13:55:25 +0900

    initial commit

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// const title = document.getElementById('title');
+// let degY = 1;
+// title.addEventListener('mouseenter', ev => {
+//   setInterval(degY => {
+//     degY = degY % 360 === 0 ? 1 : degY + 1;
+//     title.style.transform = `rotateY(${degY}deg)`;
+//   }, 1000);
+// });
